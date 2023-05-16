43335fc6ec70a71bb593f84cf85a401337d68d8d
JackPadalino <pada0867@gmail.com>
2023-05-16 07:50:18 -0400

    initial commit

---
+++ server/db/index.ts
@@ -0,0 +1,9 @@
+import db from "./db";
+import User from "./User";
+
+const data = {
+  User,
+  db,
+};
+
+export default data;
---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import app from "./app";
+
+const init = async () => {
+  try {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`listening on port ${port}`));
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+init();
