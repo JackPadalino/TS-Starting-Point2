a19c40386425c6f811b574c933925f7da6752d99
JackPadalino <pada0867@gmail.com>
2023-05-16 22:18:19 -0400

    seeded db with models

--- server/db/seed.ts
+++ server/db/seed.ts
@@ -1,24 +1,87 @@
 import db from "./db";
-import User from "./User";
+//import User from "./User";
+import { Album, Artist, Song } from ".";
+
+// All artists
+interface artist {
+  name: string;
+  genre: string;
+}
+
+// All albums
+interface album {
+  title: string;
+}
+
+// All Songs
+interface song {
+  title: string;
+  bpm: number;
+  key: string;
+}
+
+const songs: song[] = [
+  { title: "Quantum", bpm: 132, key: "5A" },
+  { title: "Headrush", bpm: 130, key: "6B" },
+  { title: "Go With the Flow", bpm: 145, key: "5A" },
+];
 
 const seed = async () => {
+  console.log("Starting seed...");
   await db.sync({ force: true });
+  try {
+    console.log("adding artists");
 
-  const [moe, lucy, larry, ethyl] = await Promise.all([
-    User.create({ username: "moe", password: "123" }),
-    User.create({ username: "lucy", password: "123" }),
-    User.create({ username: "larry", password: "123" }),
-    User.create({ username: "ethyl", password: "123" }),
-  ]);
+    const artists: artist[] = [
+      { name: "Filterheadz", genre: "Peak Time/Driving Techno" },
+      { name: "Anthony Parasole", genre: "Peak Time/Driving Techno" },
+      { name: "Jastice", genre: "Industrial Techno" },
+    ];
 
-  return {
-    users: {
-      moe,
-      lucy,
-      larry,
-      ethyl,
-    },
-  };
+    const [filterHeadz, anthonyParasole, jastice] = await Promise.all(
+      artists.map((artist) => Artist.create(artist))
+    );
+
+    console.log("adding albums");
+
+    const albums: album[] = [
+      { title: "Quantum EP" },
+      { title: "Full EFX" },
+      { title: "Acceleration Series" },
+    ];
+
+    const [filterHeadzAlbum, parasoleAlbum, jasticeAlbum] = await Promise.all(
+      albums.map((album) => Album.create(album))
+    );
+
+    console.log("adding songs");
+    const [quantumSong, headrushSong, goWithTheFlowSong] = await Promise.all(
+      songs.map((song) => Song.create(song))
+    );
+
+    //@ts-ignore
+    filterHeadz.addAlbums(filterHeadzAlbum);
+    //@ts-ignore
+    filterHeadzAlbum.addSongs(quantumSong);
+    //@ts-ignore
+    anthonyParasole.addAlbums(parasoleAlbum);
+    //@ts-ignore
+    parasoleAlbum.addSongs(headrushSong);
+    //@ts-ignore
+    jastice.addAlbums(jasticeAlbum);
+    //@ts-ignore
+    jasticeAlbum.addSongs(goWithTheFlowSong);
+
+    // const prototypeMethods = Object.getOwnPropertyNames(
+    //   Object.getPrototypeOf(filterHeadz)
+    // );
+
+    // console.log(prototypeMethods);
+  } catch (err) {
+    console.log(err);
+  }
+
+  console.log("Seed complete!");
 };
 
 seed();
