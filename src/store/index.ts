41383a9c0924181ba62ac516a0ba535ff07b3020
JackPadalino <pada0867@gmail.com>
2023-05-20 18:29:14 -0400

    finished configuring redux store - next step is to set up components for front end

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./userSlice";
-import { useDispatch } from "react-redux";
+//import userReducer from "./userSlice";
+import artistsReducer from "./artistsSlice";
+import albumsReducer from "./albumSlice";
+import songsReducer from "./songsSlice";
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    //user: userReducer,
+    artists: artistsReducer,
+    albums: albumsReducer,
+    songs: songsReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export default store;
